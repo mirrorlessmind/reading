10623cb20a054d2919775da37a73e5902ea3f3e8
Cigi <cigi@mirrorlessmind.com>
2021-04-10 23:03:36 -0500

    Added Emojis

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,45 @@
 // generateReadme function populating the README.md
 function generateReadme(answers) {
-  return ` ${answers.projectTitle} 👋</h1>
+  return `
+  <h1> ${answers.title} </h1>
 
-![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
-## Description
-🔍 ${answers.description}
+
 ## Table of Contents
+- [Use](#use)
 - [Description](#description)
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
 - [Contributing](#contributing)
+- [Installation](#installation)
+- [License](#license)
 - [Tests](#tests)
 - [Questions](#questions)
-## Installation
-${answers.installation}
 ## Usage
-${answers.usage}
-## License
-![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+${answers.usage};
+
+## Description 🔥
+${answers.description}
+
+## Contributing Partners ✨
+${answers.contributors}
+
+## Installation 💻
+${answers.install}
+
+## License 
+![badge](https://img.shields.io/badge/license-${answers.license}-blue)
 <br />
 This application is covered by the ${answers.license} license. Please refer to documention for further information.
-## Contributing
-${answers.contributing}
-## Tests
-✏️ ${answers.tests}
-## Questions
-${answers.questions}<br />
-<br />
-Visit our awesome GitHub: [${answers.username}](https://github.com/${answers.username})<br />
-<br />
-Questions? Email Me~ ${answers.email}<br /><br />
+
+## Tests Included? 😎
+${answers.test}
+
+## What do I do if I have an issue? 😔
+${answers.question}<br />
+
+## Contact Me 😻 ~ ${answers.email}<br />
+
+
+Visit our awesome GitHub : [${answers.username}](https://github.com/${answers.username})<br />
+Wanna Know More Visit My LinkedIn: [${answers.usernamelink}](https://linkedIn.com/${answers.usernamelink})<br />
   `;
 }
 
